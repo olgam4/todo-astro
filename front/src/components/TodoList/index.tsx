6fd6bc11eaca1d49eb1fb23933ae800efcf3b821
Olivier Gamache <olivier.gamache@elapsetech.com>
2022-05-25 20:29:54 -0400

    👏 Deploy?

--- front/src/components/TodoList/index.tsx
+++ front/src/components/TodoList/index.tsx
@@ -2,7 +2,7 @@ import { createEffect } from "solid-js";
 
 export default function () {
   createEffect( () => {
-    const events = new EventSource('/api/todo-stream');
+    const events = new EventSource('http://localhost:8000/see');
     events.onmessage = (event) => {
       const parsedData = JSON.parse(event.data);
       console.log(parsedData)
