6267ac0677fad197b8b70ac2643b01bf4499c93e
Olivier Gamache <olivier.gamache@elapsetech.com>
2022-05-26 13:07:40 -0400

    😀 Modify the colors

--- front/src/components/Form/index.tsx
+++ front/src/components/Form/index.tsx
@@ -56,7 +56,7 @@ export default function () {
           />
         <button
           type="submit"
-          class="absolute right-3 top-8 h-10 w-10 transition-all rounded-full flex justify-center items-center bg-blue-200 hover:bg-blue-700 hover:text-white"
+          class="shadow-xl shadow-black-900 absolute right-3 top-8 h-10 w-10 transition-all rounded-full flex justify-center items-center bg-blue-300 hover:bg-blue-700 hover:text-white"
         >
           <AddIcon />
         </button>
