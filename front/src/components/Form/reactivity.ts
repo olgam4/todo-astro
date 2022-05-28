a76b07eaae41d8355189527df869cea945896de7
Olivier Gamache <olivier.gamache@elapsetech.com>
2022-05-28 03:09:10 -0400

    🛌  Testing why the 500 in prod

--- front/src/components/Form/reactivity.ts
+++ front/src/components/Form/reactivity.ts
@@ -1,18 +1,9 @@
 import { createSignal } from 'solid-js'
 
-const submit = (ref, accessor) => {
-  const callback = accessor() || (() => {});
-  ref.onsubmit = async (e) => {
-    e.preventDefault();
-    callback(ref)
-  }
-}
-
 const createTodo = () => {
   return createSignal('')
 }
 
 export {
   createTodo,
-  submit,
 }
